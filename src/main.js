f723cc9e78dd3ba23ec5f614f3daad649469717d
blling <blling.fa@foxmail.com>
2018-03-21 16:01:06 +0800

    Fix hot load

--- src/main.js
+++ src/main.js
@@ -42,10 +42,7 @@ if (module.hot) {
   // Hot reloadable React components and translation json files
   // modules.hot.accept does not accept dynamic dependencies,
   // have to be constants at compile-time
-  module.hot.accept(['containers/App'], () => {
-    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
-    render();
-  });
+  module.hot.accept(['containers/App']);
 }
 
 render();
